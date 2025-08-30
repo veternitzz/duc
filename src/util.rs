01fe4d5f61af66ba222ec12ea8d10c8df310853b
veternitzz <anthonylapeter@gmail.com>
2025-08-30 11:41:41 -0400

    re-factored util::module_exists() to return a ModuleKind

--- src/util.rs
+++ src/util.rs
@@ -1,18 +1,28 @@
 use std::fs;
 
+#[derive(PartialEq)]
+pub enum ModuleKind {
+    SIMPLE,
+    COMPLEX,
+    NONE
+}
+
 /// Searches the standard source code directory (``./src/``) for the specified Luau module.
-/// A Luau module in this case is considered to be either a standalone file or a
-/// large module where the path would be ``./src/module/init.luau``. If the module
-/// was named ``foo`` the path would either be ``./src/foo.luau`` or it would be ``./src/foo/init.luau``.
-pub fn module_exists(name: &'static str) -> bool {
+/// A Luau module in this case is considered to be either a simple module or a
+/// complex module where the path would be ``./src/module/init.luau``. If the module
+/// was named ``foo`` the path would either be ``./src/foo.luau`` for a simple module or it would be
+/// ``./src/foo/init.luau`` for a complex module. If it finds a module matching the name provided,
+/// it either returns ``ModuleKind::SIMPLE``, ``ModuleKind::COMPLEX`` and if it does not find one
+/// it returns ``ModuleKind::NONE``.
+pub fn module_exists(name: &'static str) -> ModuleKind {
     let basic_path = format!("./src/{}.luau", name);
     let large_path = format!("./src/{}/init.luau", name);
 
     if fs::exists(basic_path).unwrap() == true {
-        return true
+        return ModuleKind::SIMPLE
     } else if fs::exists(large_path).unwrap() == true {
-        return true
+        return ModuleKind::COMPLEX
     } else {
-        return false
+        return ModuleKind::NONE
     }
 }
