7dfe5db84274c80bce3a1a4d354e9ced268cb309
veternitzz <anthonylapeter@gmail.com>
2025-09-06 19:14:39 -0400

    add std_sys to require resolver

--- src/std_lib/std_globals/require.rs
+++ src/std_lib/std_globals/require.rs
@@ -19,6 +19,7 @@ fn get_std_library(luau: &Lua, path: String) -> LuaResult<LuaTable> {
         "@std" => Ok(std_lib::create(&luau)?),
         "@std/fs" => Ok(std_fs::create(&luau)?),
         "@std/io" => Ok(std_io::create(&luau)?),
+        "@std/sys" => Ok(std_sys::create(&luau)?),
         &_ => Err(LuaError::RuntimeError(String::from("invalid standard library")))
     }
 }
