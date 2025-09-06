b854bd1cc55a137e4af4fadfad39581953f0b7d7
veternitzz <anthonylapeter@gmail.com>
2025-09-06 19:15:17 -0400

    implement sys.hostname()

--- src/std_lib/std_sys.rs
+++ src/std_lib/std_sys.rs
@@ -1,8 +1,19 @@
+use std::os::unix::ffi::OsStrExt;
+
 use mlua::prelude::*;
+
+use gethostname::gethostname;
 
 pub fn create(luau: &Lua) -> LuaResult<LuaTable> {
     let table = luau.create_table()?;
+    table.set("hostname", luau.create_function(sys_host_name)?)?;
     table.set_readonly(true);
 
     Ok(table)
+}
+
+fn sys_host_name(luau: &Lua, (): ()) -> LuaResult<LuaString> {
+    let host_name = gethostname();
+    
+    Ok(luau.create_string(host_name.as_bytes())?)
 }
