ea1f3b807f58cd208e5e9334f802db5e04d8ffb3
veternitzz <anthonylapeter@gmail.com>
2025-09-06 19:57:30 -0400

    implement sys.name()

--- src/std_lib/std_sys.rs
+++ src/std_lib/std_sys.rs
@@ -5,6 +5,7 @@ use sysinfo::{System};
 pub fn create(luau: &Lua) -> LuaResult<LuaTable> {
     let table = luau.create_table()?;
     table.set("hostname", luau.create_function(sys_host_name)?)?;
+    table.set("name", luau.create_function(sys_name)?)?;
     table.set_readonly(true);
 
     Ok(table)
@@ -14,4 +15,10 @@ fn sys_host_name(luau: &Lua, (): ()) -> LuaResult<LuaString> {
     let host_name = System::host_name().unwrap();
 
     Ok(luau.create_string(host_name.as_bytes())?)
+}
+
+fn sys_name(luau: &Lua, (): ()) -> LuaResult<LuaString> {
+    let name = System::name().unwrap();
+
+    Ok(luau.create_string(name.as_bytes())?)
 }
