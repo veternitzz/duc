39d0abd20bb347930c6ad7ba29f04d77ac8999eb
veternitzz <anthonylapeter@gmail.com>
2025-09-06 19:07:04 -0400

    setup new std_sys module

--- src/std_lib/mod.rs
+++ src/std_lib/mod.rs
@@ -1,6 +1,7 @@
 pub mod std_fs;
 pub mod std_io;
 pub mod std_globals;
+pub mod std_sys;
 
 use mlua::prelude::*;
 
@@ -8,6 +9,7 @@ pub fn create(luau: &Lua) -> LuaResult<LuaTable> {
     let table = luau.create_table()?;
     table.set("fs", std_fs::create(&luau)?)?;
     table.set("io", std_io::create(&luau)?)?;
+    table.set("sys", std_sys::create(&luau)?)?;
 
     Ok(table)
 }
